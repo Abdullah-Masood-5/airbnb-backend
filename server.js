d06941eb6cfce6ef8d8e0090c8e771c7f6f72c49
Abdullah Masood <bscs22054@itu.edu.pk>
2024-12-03 00:44:54 +0500

    Add authentication backend

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
-const connectDB = require('./mongoDb/db');
-const authRoutes = require('./routes/auth');
-const path = require('path');
+const connectDB = require('./mongoDb/db');;
 
 
 dotenv.config();
@@ -13,21 +11,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use('/images', express.static(path.join(__dirname, 'images')));
+
 
 app.use('/api/auth', authRoutes);
-const profileRoutes = require('./routes/profile');
-app.use('/api/profile', profileRoutes);
 
-
-const adminRoutes = require('./routes/admin');
-app.use('/api/admin', adminRoutes);
-app.use('/api/', require('./routes/clientRoutes'));
-
-
-app.use('/api/bookings',  require('./routes/bookingRoutes'));
-
-app.use('/api/admin-bookings',  require('./routes/adminBookingRoutes'));
 
 
 const PORT = process.env.PORT || 5000;

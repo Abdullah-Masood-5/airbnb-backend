f9d5574e008a32503d59b1d220eef07ec617bceb
Abdullah Masood <bscs22054@itu.edu.pk>
2024-12-04 23:23:56 +0500

    Complete evrything else that bookings

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const cors = require('cors');
-const connectDB = require('./mongoDb/db');
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const connectDB = require("./mongoDb/db");
 const authRoutes = require("./routes/auth");
-const path =  require('path');
+const path = require("path");
 
 dotenv.config();
 connectDB();
@@ -12,17 +12,17 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use("/images", express.static(path.join(__dirname, "images")));
 
-app.use('/api/auth', authRoutes);
-const profileRoutes = require('./routes/profile');
-app.use('/api/profile', profileRoutes);
+app.use("/api/auth", authRoutes);
+const profileRoutes = require("./routes/profile");
+app.use("/api/profile", profileRoutes);
 
+const adminRoutes = require("./routes/admin");
 
-const adminRoutes = require('./routes/admin');
-app.use('/api/admin', adminRoutes);
-app.use('/api/', require('./routes/clientRoutes'));
-const profileRoutes = require('./routes/profile');
+app.use("/api/admin", adminRoutes);
+
+app.use("/api/", require("./routes/clientRoutes"));
 
 
 const PORT = process.env.PORT || 5000;
